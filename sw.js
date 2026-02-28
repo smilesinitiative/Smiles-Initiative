e7f870dbc221f35ada588fa4ed31614704646b4a
smilesinitiative <smilesinitiative254@gmail.com>
2026-02-28 08:29:28 +0300

    Refactor service worker caching logic and syntax

--- sw.js
+++ sw.js
@@ -1,24 +1,22 @@
-const cacheName = 'smiles-cache-v1';
+const cacheName = "smiles-cache-v1";
 
-const filesToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './smileslogo.jpg'
+const assets = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./smileslogo.jpg"
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(filesToCache);
-    })
+    caches.open(cacheName).then(cache => cache.addAll(assets))
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
     })
   );
 });
