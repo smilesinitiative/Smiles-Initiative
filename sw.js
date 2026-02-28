4546a834d673e2c4395f3c137e31c867c4f469a8
smilesinitiative <smilesinitiative254@gmail.com>
2026-02-28 07:43:03 +0300

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,24 @@
+const cacheName = 'smiles-cache-v1';
+
+const filesToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './smileslogo.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
